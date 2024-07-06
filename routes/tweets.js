2523e74ce163736a8afc4e978238ef7e03afec72
fchery87 <frantzchery@hotmail.com>
2024-07-06 18:24:29 -0400

    updated to tweet organizing

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,3 +1,4 @@
+// routes/tweets.js
 import express from "express";
 import multer from "multer";
 import User from "../models/User.js";
@@ -21,24 +22,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-/**
- * Create a new Tweet
- * @method POST /tweets/
- * @description This route is used by the addTweet function in TweetList.jsx
- */
 router.post("/", upload.single('image'), async (req, res) => {
-  console.log("Request body:", req.body);
-  console.log("Request file:", req.file);
-
   const { username, content } = req.body;
   const imagePath = req.file ? req.file.path : null;
 
-  // check if user exists
   const dbUser = await User.findOne({ username });
 
   if (dbUser) {
-    console.log(dbUser);
-
     const tweet = await Tweet.create({
       content,
       user: dbUser._id,
@@ -48,55 +38,39 @@ router.post("/", upload.single('image'), async (req, res) => {
     return res.json(tweet);
   } else {
     const newUser = await User.create({ username });
-    console.log(newUser);
-
     const tweet = await Tweet.create({
       content,
       user: newUser._id,
       username: newUser.username,
       image: imagePath,
     });
-
     return res.json(tweet);
   }
 });
 
-/**
- * Fetch all tweets
- * @method GET /tweets/
- * @description This route is used by the useEffect in TweetList.jsx
- */
 router.get("/", async (req, res) => {
   try {
-    const tweets = await Tweet.find();
+    const tweets = await Tweet.find().sort({ createdAt: -1 }); // Sort by creation date in descending order
     res.send(tweets);
   } catch (error) {
     res.json({ msg: error.message });
   }
 });
 
-/**
- * Deletes tweet by the id
- * @method Delete /tweets/:id
- * @param id
- * @description This route is used by the removeTweet in TweetList.jsx
- */
+
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    await Tweet.findOneAndDelete(id);
+    const deletedTweet = await Tweet.findByIdAndDelete(id);
+    if (!deletedTweet) {
+      return res.status(404).json({ msg: "Tweet not found" });
+    }
     res.json({ msg: `Tweet with id: ${id} was deleted!` });
   } catch (error) {
-    res.json({ msg: error.message });
+    res.status(500).json({ msg: error.message });
   }
 });
 
-/**
- * Updates tweets by the id
- * @method PUT /tweets/:id
- * @param id
- * @description This route is used by the updateTweet in TweetList.jsx
- */
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { newTweetContent } = req.body;
@@ -107,17 +81,12 @@ router.put("/:id", async (req, res) => {
       { new: true }
     );
 
-    console.log(updatedTweet);
     res.json(updatedTweet);
   } catch (error) {
-    res.json({ msg: error.message });
+    res.status(500).json({ msg: error.message });
   }
 });
 
-/**
- * Like a tweet
- * @method PATCH /tweets/:id/like
- */
 router.patch("/:id/like", async (req, res) => {
   const { id } = req.params;
   try {
@@ -133,10 +102,6 @@ router.patch("/:id/like", async (req, res) => {
   }
 });
 
-/**
- * Retweet a tweet
- * @method PATCH /tweets/:id/retweet
- */
 router.patch("/:id/retweet", async (req, res) => {
   const { id } = req.params;
   try {
